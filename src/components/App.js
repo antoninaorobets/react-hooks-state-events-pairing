d36ba205fc171975c795b30e7a9f33be1b0c7c9b
antoninaorobets <pani.antonina.don@gmail.com>
2022-01-26 22:41:24 -0500

    question here

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,23 @@
+import React,{useState} from "react";
 import video from "../data/video.js";
+import Video from "./Video.js";
+import Info from "./Info.js";
+import Reactions from "./Reactions.js";
+import ComentsList from "./ComentsList.js";
+import Button from "./Button.js";
 
 function App() {
   console.log("Here's your data:", video);
+  const [showComments,setShowComments]=useState(true)
 
   return (
     <div className="App">
-      <iframe
-        width="919"
-        height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-        frameBorder="0"
-        allowFullScreen
-        title="Thinking in React"
-      />
+    <Video />
+    <Info title={video.title} views={video.views} createdAt={video.createdAt} />
+    <Reactions />
+    <Button showComments={showComments} setShowComments={setShowComments} />
+    {showComments ? <ComentsList comments={video.comments}/> : null}
+    
     </div>
   );
 }
